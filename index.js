ea399c010257a02f884baa3c18ccf7557e5a3066
jppaalas <jp.paalassalo@gmail.com>
2021-02-05 04:37:04 +0000

    v4

--- index.js
+++ index.js
@@ -1,8 +1,19 @@
 const express = require('express');
 const path = require('path');
+const moment = require('moment');
+
 const app = express();
+
+// middleware
+const logger = (req, res, next) => {
+    console.log(`${req.protocol}://${req.get('host')}${req.originalUrl}: ${moment().format()}`)
+    //console.log('Hello');
+    next();
+}
+app.use(logger);
+app.use(express.static(path.join(__dirname, 'public')));
+// app.get('/', (req,res) => {
+//  res.send('<h1>Hello W2</h1>');
+//});
 const PORT = process.env.port || 5000
-app.get('/', (req,res) => {
-  res.send('<h1>Hello W2</h1>');
-});
 app.listen(PORT, () => console.log('Server started'));
