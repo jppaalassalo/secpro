d8e6bdea1a7cc3947898a661d32909129c959625
JPP <jp@prgramed.fi>
2021-04-04 07:37:57 +0300

    added home route to allow for other pages later

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeModule } from './home/home.module';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'home', component: HomeModule },
+  { path: '',   redirectTo: '/home', pathMatch: 'full' }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
